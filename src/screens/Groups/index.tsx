ecf412be6333ee2f8cfdba254d988929760b1120
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-03-13 19:58:44 -0300

    Course Init and Setup

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as S from "@screens/Groups/styles";
 
 export function Groups() {
     return (
-        <View style={styles.container}>
-            <Text>Groups</Text>
-        </View>
+        <S.Container>
+            <S.Title>Groups</S.Title>
+        </S.Container>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+// });
